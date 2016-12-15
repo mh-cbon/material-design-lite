8160067edbec1e0ca86af1d085085fc93b079ff2
mh-cbon <mh-cbon@users.noreply.github.com>
2016-12-15 15:05:25 +0100

    fix params overrides

--- src/custom-form-ajax/form-ajax.js
+++ src/custom-form-ajax/form-ajax.js
@@ -447,8 +447,6 @@
    */
   CustomFormAjax.prototype.onSubmit_ = function(ev) {
     ev.preventDefault();
-    ev.stopPropagation();
-    ev.stopImmediatePropagation();
     this.handleSubmit_();
   };
   /**
